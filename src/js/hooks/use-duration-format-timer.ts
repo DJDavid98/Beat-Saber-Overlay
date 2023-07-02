082d1c225b2efcce0b1d683f15c2772ba1bf8359
DJDavid98 <djdavid98@protonmail.com>
2023-07-02 17:35:12 +0200

    Fix duration displays using hours

--- src/js/hooks/use-duration-format-timer.ts
+++ src/js/hooks/use-duration-format-timer.ts
@@ -3,5 +3,4 @@ import DurationUnitFormat from 'intl-unofficial-duration-unit-format';
 
 export const useDurationFormatTimer = () => useMemo(() => new DurationUnitFormat('en-US', {
     style: 'timer',
-    format: '{hour}:{minutes}:{seconds}'
 }), []);
